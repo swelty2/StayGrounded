eb076f4593a0fde87298b5a1de90e6f80f021406
Durga Abayakumar <42677554+durgaabhay@users.noreply.github.com>
2019-04-08 16:09:26 -0400

    adding d3 charts

--- stay_grounded/src/components/welcome.vue
+++ stay_grounded/src/components/welcome.vue
@@ -1,39 +1,27 @@
 <template>
-  <div class="container-fluid">
-    <h1 class="site-heading text-center text-white d-none d-lg-block">
-      <span class="site-heading-upper text-primary mb-3">{{ title }}</span>
-    </h1>
-    <!--<div class="text-center font-italic text-primary">
-      Lorem ipsum dolor sit amet. Phasellus tincidunt pulvinar dapibus diam mollis urna, eleifend mauris donec litora rhoncus nisl, porta nibh velit accumsan justo. Senectus mollis dictum elementum rutrum congue scelerisque varius conubia, tristique justo sagittis vestibulum duis dapibus convallis, nec vel orci curabitur ad fusce lectus.
-      <form class="form-inline">
-        <div class="form-group mb-2">
-          <input type="text" readonly class="form-control-plaintext" value="Enter your name">
-        </div>
-        <div class="form-group mx-sm-3 mb-2">
-          <label class="sr-only">User Name</label>
-          <input v-model="userName" class="form-control" id="userName" placeholder="Name">
-        </div>
-      </form>
-    </div>-->
-    <router-link class="btn btn secondary"  to="/home/Donna">Explore</router-link>
-    <router-view></router-view>
-    <figure class="figure">
-      <img src="../assets/coffee-beans.jpg" class="intro-img img-fluid mb-3 mb-lg-0 rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-      <figcaption class="figure-caption text-center">Freshly brewed</figcaption>
-    </figure>
+
+  <div class="container">
+    <img src=
+           "../assets/logo.jpg"  alt="Stay Grounded Logo Image" class="image" >
+    <div class="middle">
+      <div class="text">
+        <router-link class="text"  to="/home/Donna">Explore</router-link>
+        <router-view></router-view></div>
+    </div>
   </div>
+
 </template>
 
 <script>
-    export default {
-        name: "welcome",
-        data() {
-          return {
-            userName: '',
-            title : 'Stay Grounded - A unique experience'
-          }
-        }
+  export default {
+    name: "welcome",
+    data() {
+      return {
+        userName: '',
+        title : 'Stay Grounded - A unique experience'
+      }
     }
+  }
 </script>
 
 <style scoped>
--- stay_grounded/src/main.js
+++ stay_grounded/src/main.js
@@ -7,8 +7,12 @@ import welcome from './components/welcome'
 import menu from './components/menu'
 import brew from './components/brew'
 import design from './components/design'
+import coffeeChart from './components/coffee-chart'
+import teaChart from './components/tea-chart'
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.use(Router);
+Vue.component('apexchart', VueApexCharts);
 
 const routes = [
   { path : '/', component: welcome },
@@ -16,7 +20,9 @@ const routes = [
   { path: '/home/:userName', component: home},
   { path: '/menu', component: menu},
   { path: '/brew/:drinkName', component: brew},
-  { path: '/buildDrink', component: design}
+  { path: '/buildDrink', component: design},
+  { path: '/coffee-chart', component: coffeeChart},
+  { path: '/tea-chart', component: teaChart}
 ];
 
 const router = new Router({
