d7daf1cdd914c04f2c5e7389559779330a96ee3b
Sarah Welty <43446381+swelty2@users.noreply.github.com>
2019-04-18 19:00:11 -0400

    Add files via upload

---
+++ StayGrounded-master/stay_grounded/src/main.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import BootStrapVue from 'bootstrap-vue'
+import VueApexCharts from 'vue-apexcharts'
+
+import App from './App'
+import home from './components/home'
+import welcome from './components/welcome'
+import menu from './components/menu'
+import brew from './components/brew'
+import design from './components/design'
+import data from './components/data'
+import speechform from './components/speechform'
+
+
+Vue.use(Router);
+Vue.component('apexchart', VueApexCharts);
+Vue.use(BootStrapVue);
+
+const routes = [
+  { path : '/', component: welcome },
+  { path: '/home', component: home},
+  { path: '/home/:userName', component: home},
+  { path: '/menu', component: menu},
+  { path: '/brew', component: brew},
+  { path: '/brew/:drinkName', component: brew},
+  { path: '/buildDrink', component: design},
+  { path: '/data', component: data},
+  { path: '/speechform', component: speechform}
+
+];
+
+const router = new Router({
+  routes
+});
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});
